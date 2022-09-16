b38dbaad4bd80c320543255976b32eed34f9196b
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2022-09-16 22:47:55 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,10 +2,14 @@ import React from "react"
 
 const AddTodo = ({ dispatch }) => {
 
-
-    return (
-        <></>
-    )
+const [text, setText] = useState("");
+  return (
+    <div className="AddTodo">
+      <input value={text} onChange={e => setText(e.target.value)} className="AddTodoInput" />
+      <button className="AddTodoButton" onClick={() => {add(text); setText("")}}>Add</button>
+    </div>
+  );
+   
 }
 
 export { AddTodo }
